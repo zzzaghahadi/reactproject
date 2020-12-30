79a59f27df57d7456c0dd2e8cb50bc52ff5bdebe
fathemeh razavian poor <fatemeh_rz98@yahoo.com>
2020-12-31 01:00:09 +0330

    fetch data component added

---
+++ src/components/FetchData.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+
+export default class FetchData extends Component {
+  state = {
+    data: [],
+    Sell: [],
+    Buy: [],
+  };
+  componentDidMount() {
+    const fetch = [];
+    Axios.get(
+      "https://gist.githubusercontent.com/ahmadpanah/e6225b11235a04397b813d0de6e6f5ad/raw/ab559a1146b5e6f2d57bd546c20c090842d5fcc6/sana.json"
+    ).then((res) => {
+      console.log(res);
+      for (let key in res.data.sana.data) {
+        fetch.push({
+          ...res.data.sana.data[key],
+          id: key,
+        });
+      }
+      this.setState({ data: fetch });
+    });
+  }
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
